4c7fbe1d9eaf90348e8e82cc1c9c8f70652a3cfd
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-05-30 08:53:28 +0700

    adjust back button icon

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/navigation";
+import { IoArrowBack } from "react-icons/io5";
 
 export default function BackButton() {
   const router = useRouter();
   return (
-    <button className="self-start text-lg" onClick={() => router.back()}>Back</button>
+    <button className="self-start text-lg" onClick={() => router.back()}>
+      <IoArrowBack size={30}/>
+    </button>
   )
 };
